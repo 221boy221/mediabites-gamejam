6793b431b626fe92c76bfe19bfe2b6210fd958ec
kevinboogaard <kevin.boogaard3@gmail.com>
2015-05-28 21:44:44 +0200

    Tussen Push - Kevin
    -Ui upgreat

--- Assets/Scripts/UIMap/UIScript.cs
+++ Assets/Scripts/UIMap/UIScript.cs
@@ -6,15 +6,39 @@ public class UIScript : MonoBehaviour {
 
     [SerializeField]
     private GameObject panel;
+    [SerializeField]
+    private GameObject UIPanel;
+    [SerializeField]
+    private GameObject UpgradeButton;
 	void Update () 
-    {	
-        if(Input.GetKeyDown("e") && panel.active == false)
+    {
+
+        if (Input.GetKeyDown("e") && panel.active == false)
         {
             panel.SetActive(true);
         }
         else if (Input.GetKeyDown("e") && panel.active == true)
         {
             panel.SetActive(false);
+            UpgradeButton.SetActive(true);
+            UIPanel.SetActive(false);
+
+        }
+    }
+
+    public void OpenShop()
+    {
+        Debug.Log("openshop");
+        if (UIPanel.active == true)
+        {
+            UIPanel.SetActive(false);
+            UpgradeButton.SetActive(true);
+        }
+
+        else if(UIPanel.active == false)
+        {
+            UIPanel.SetActive(true);
+            UpgradeButton.SetActive(false);            
         }
     }
 }
