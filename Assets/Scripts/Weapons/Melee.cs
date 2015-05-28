a40486c5be8654065e35b0e7463ae88f22995b0b
Boy Voesten <warrock.boy@hotmail.com>
2015-05-29 00:58:46 +0200

    Added prototype weapon system

---
+++ Assets/Scripts/Weapons/Melee.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Melee : Weapon {
+
+	private float _fireDelay;
+    private float _timeSinceLastShot;
+    private GameObject _owner;
+
+    public void SetValues(float fireDelay, GameObject owner) {
+        _owner = owner;
+        _fireDelay = fireDelay;
+    }
+
+    override public void Shoot() {
+        if (Time.time > _fireDelay + _timeSinceLastShot) {
+            Attack();
+        }
+    }
+
+    private void Attack() {
+        Debug.Log("Melee - Attack");
+        // ToDo: Melee attack
+
+        _timeSinceLastShot = Time.time;
+    }
+
+}
---
+++ Assets/Scripts/Weapons/Ranged.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ranged : Weapon {
+
+    private float _fireDelay;
+    private float _timeSinceLastShot;
+    private GameObject _owner;
+    [SerializeField]
+    private GameObject _projectile;
+
+    public void SetValues(float fireDelay, GameObject owner) {
+        _owner = owner;
+        _fireDelay = fireDelay;
+    }
+
+    override public void Shoot() {
+        if (Time.time > _fireDelay + _timeSinceLastShot) {
+            Fire();
+        }
+    }
+
+    private void Fire() {
+        Debug.Log("Ranged - Fire");
+        GameObject projectile = (GameObject)GameObject.Instantiate(_projectile, _owner.transform.position, Quaternion.identity);
+        _timeSinceLastShot = Time.time;
+    }
+
+}
