add99cfb49225eb0769bc2ddac97468acb0cbb65
BeauArthurs <BeauArthurs@gmail.com>
2015-05-29 12:11:20 +0200

    b5

---
+++ Assets/canvas.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class canvas : MonoBehaviour {
+    public Text resource;
+    public GameObject player;
+	void Update () 
+    {
+	    resource.text = player.GetComponent<PlayerResources>().leaves.ToString();
+	}
+}
